61a9fdf9757741d4d2824d32d70b3faffaa5e152
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-14 23:46:26 +0400

    PartialClasses

---
+++ Classes/PartialClasses/PartialClass_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+    // Первая часть класса.
+    partial class PartialClass
+    {
+        public void MethodFromPartialClass1()
+        {
+            Console.WriteLine("Partial class 1");
+        }
+    }
+}
---
+++ Classes/PartialClasses/PartialClass_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+    // Вторая часть класса.
+    partial class PartialClass
+    {
+        public void MethodFromPartialClass2()
+        {
+            Console.WriteLine("Partial class 2");
+        }
+    }
+}
