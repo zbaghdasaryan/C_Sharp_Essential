758a2c4d7df03779f64eb2e1c648a5c70e1fa238
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-15 00:35:44 +0400

    PartialMethods

---
+++ Classes/PartialMethods/PartialClass_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PartialClasses
+{
+    // Первая часть класса.
+    partial class PartialClass
+    {
+        // Объявление частичного метода.        
+        partial void PartialMethod();
+
+        partial void MyMethod();
+    }
+}
---
+++ Classes/PartialMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Частичные методы (Partial methods).
+
+namespace PartialClasses
+{
+    class Program
+    {
+        static void Main()
+        {
+            PartialClass instance = new PartialClass();
+
+            instance.CallPartialMethod();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
