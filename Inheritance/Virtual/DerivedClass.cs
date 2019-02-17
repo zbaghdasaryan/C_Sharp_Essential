78281365176a7326fdc25771228f3502d22870b0
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-17 22:35:19 +0400

    base.Method

---
+++ Inheritance/Virtual/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class DerivedClass : BaseClass
+    {
+        // Переопределение метода базового класса.
+        public override void Method()
+        {
+            Console.WriteLine("Method from DerivedClass");
+        }
+    }
+}
