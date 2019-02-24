c2f667ad19d9f06272e3425111edfc08a263f1bb
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-24 14:50:11 +0400

    StaticConstructor2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class A
+    {
+        private int b;
+        public static int z;
+
+        static A()
+        {
+            Console.WriteLine("staticheski konstuktor");
+            z = 222;
+        }
+
+        public A(int b)
+        {
+            this.b = b;
+        }
+        public static void Msas()
+        {
+
+        }
+        public void M()
+        {
+            Console.WriteLine("{0}, {1}",b, z);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            A.Msas();
+            A.z=2;
+            Console.ReadKey();
         }
     }
 }
