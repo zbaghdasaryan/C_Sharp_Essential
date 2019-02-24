b7656578cc507e710dbfdebc4752631f6ae689dc
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-24 20:27:46 +0400

    Structures

---
+++ Structures/Structure1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Структуры.
+
+// В структурах нельзя инициализировать поля непосредственно в месте создания.
+
+namespace Structure
+{
+    struct MyStruct
+    {
+        public int field;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Создание экземпляра структурного типа, без вызова конструктора.
+            MyStruct instance;
+
+            instance.field = 1; // Закомментировать.
+
+            // Попытка вывода значения неинициализированного поля приведет к ошибке.
+            Console.WriteLine(instance.field);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
