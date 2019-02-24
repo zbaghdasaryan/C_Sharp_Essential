55da71239c03a30903f63f469150a7a5c535d170
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-24 13:57:30 +0400

    Static3

---
+++ StaticClasses/Static3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Статические члены в нестатических классах.
+
+namespace Static
+{
+    class Program
+    {
+        static void Main()
+        {
+            NotStaticClass.Property = 3;
+
+            Console.WriteLine(NotStaticClass.Property);
+            
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
