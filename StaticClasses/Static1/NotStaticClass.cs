8daded2079f257ed3a3b4b09f8e04930818a240d
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-24 13:37:49 +0400

    Static1

---
+++ StaticClasses/Static1/NotStaticClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Static
+{
+    class NotStaticClass
+    {
+        private int Id;
+        public static int field;  
+
+        // Конструктор.
+        public NotStaticClass(int Id)
+        {
+            this.Id = Id;
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Instance{0}.field = {1}", Id, field);
+        }
+
+    }
+}
