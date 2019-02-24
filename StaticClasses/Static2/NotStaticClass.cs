ac86e015382a154ba25ffb1860f49581222156ba
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-24 13:44:22 +0400

    Constant

---
+++ StaticClasses/Static2/NotStaticClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// В статических методах нельзя обращаться к нестатическим полям.
+
+namespace Static
+{
+    class NotStaticClass
+    {
+        private int id;
+
+        // Конструктор.
+        public NotStaticClass(int id)
+        {
+            this.id = id;
+        }
+
+        public static void Method()
+        {
+            //Console.WriteLine("Instance.Id = {0}", Id);
+
+            Console.WriteLine("В статических методах нельзя обращаться к нестатическим полям.");
+        }
+    }
+}
